306b255b86fec2046611b2ea046fa49ae7ddba9a
Ivan Ivanov <me@ivanov.pro>
2022-12-01 21:17:55 +0200

    Update crawer.py

--- crawer.py
+++ crawer.py
@@ -7,7 +7,7 @@ DB = MONGO_CLIENT["fmi-project"]
 
 movies = DB["movies"]
 
-class BlogSpider(scrapy.Spider):
+class MovieSpider(scrapy.Spider):
     name = 'imdb_top_movies'
     start_urls = ['https://www.imdb.com/chart/top/']
 
