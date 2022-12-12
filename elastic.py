35846bba7a4c3a60bc3bc9d2dc1430c2ef803cff
Ivan Ivanov <me@ivanov.pro>
2022-12-12 22:53:43 +0200

    add sample search logic

---
+++ elastic.py
@@ -0,0 +1,23 @@
+from elasticsearch import Elasticsearch
+
+ELASTIC_PASSWORD = 'RRfdxagvI1HKL9l+5Pd1'
+ELASTIC_FINGERPRINT = '34752822c2815921bc7f159894d82546ad4e076b0da60561a2269d018061f703'
+
+elastic = Elasticsearch(
+    hosts='https://localhost:9200',
+    ssl_assert_fingerprint = ELASTIC_FINGERPRINT,
+    basic_auth = ( 'elastic', ELASTIC_PASSWORD )
+)
+
+#print(es.info())
+#print(elastic.indices.create(index='movies'))
+#print(es.count(index='movies'))
+
+#print(elastic.search(index="movies", query={"match_all": {}}))
+
+def search_by_title(title: str):
+    return elastic.search(index='movies', query= {
+        'match': {
+            'title': title
+        }
+    })
--- imdb_spider/imdb_spider/spiders/spider.py
+++ imdb_spider/imdb_spider/spiders/spider.py
@@ -1,6 +1,20 @@
 from scrapy import Spider
 from datetime import datetime
 
+from elasticsearch import Elasticsearch
+
+ELASTIC_PASSWORD = 'RRfdxagvI1HKL9l+5Pd1'
+ELASTIC_FINGERPRINT = '34752822c2815921bc7f159894d82546ad4e076b0da60561a2269d018061f703'
+
+es = Elasticsearch(
+    "https://localhost:9200",
+    ssl_assert_fingerprint = ELASTIC_FINGERPRINT,
+    basic_auth = (
+        "elastic", 
+        ELASTIC_PASSWORD
+    )
+)
+
 class MovieSpider(Spider):
     name = 'imdb_movies'
     start_urls = ['https://www.imdb.com/chart/top']
@@ -31,5 +45,7 @@ class MovieSpider(Spider):
             story
             keywords
         """
+
+        print(es.index(index='movies', document=movie_doc))
     
         yield movie_doc
