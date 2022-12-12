076a6f41512a1a3a6aabf34733b0938503bca1a2
Ivan Ivanov <me@ivanov.pro>
2022-12-12 23:35:25 +0200

    create sample web part project

---
+++ project/web/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        query = request.form['query']
+
+        return render_template('index.html', query=query)
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
